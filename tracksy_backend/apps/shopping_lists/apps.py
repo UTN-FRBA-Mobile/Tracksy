5a7a9e14df76a72b916958e3794801645dd86cda
nmolina02 <nicomolina1310@gmail.com>
2026-06-14 13:00:49 -0300

    Fixeo en migración de estado de prodcuto y en login panel admin

--- tracksy_backend/apps/shopping_lists/apps.py
+++ tracksy_backend/apps/shopping_lists/apps.py
@@ -1,7 +1,20 @@
 from django.apps import AppConfig
 
 
+ESTADOS_DEFAULT = ["Pendiente", "Comprado", "No disponible"]
+
+
+def create_estados_default(sender, **kwargs):
+    from apps.shopping_lists.models import EstadoProducto
+    for nombre in ESTADOS_DEFAULT:
+        EstadoProducto.objects.get_or_create(nombre=nombre)
+
+
 class ShoppingListsConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "apps.shopping_lists"
     verbose_name = "Shopping Lists"
+
+    def ready(self):
+        from django.db.models.signals import post_migrate
+        post_migrate.connect(create_estados_default, sender=self)
